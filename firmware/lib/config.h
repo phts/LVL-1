9706fac011d264243535195f705c77f4335b5665
Phil Tsaryk <phil@tsaryk.com>
2024-11-21 14:40:44 +0100

    feat: change pins

--- firmware/lib/config.h
+++ firmware/lib/config.h
@@ -4,9 +4,9 @@
 class Config
 {
 public:
-  static const byte PIN_INDICATOR = 3;
-  static const byte PIN_ULTRASONIC_SENSOR_TRIGGER = 5;
-  static const byte PIN_ULTRASONIC_SENSOR_ECHO = 6;
+  static const byte PIN_INDICATOR = 6;
+  static const byte PIN_ULTRASONIC_SENSOR_TRIGGER = 3;
+  static const byte PIN_ULTRASONIC_SENSOR_ECHO = 4;
   static const byte PIN_BTN_CHECK = 8;
   static const byte PIN_LED_WARNING = 11;
 };
