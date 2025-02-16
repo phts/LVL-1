34e52ccb157edd717ac33eba7e17a1357e5ce793
Phil Tsaryk <phil@tsaryk.com>
2025-02-16 23:12:30 +0100

    feat: change pins

--- arduino/lib/config.h
+++ arduino/lib/config.h
@@ -4,13 +4,13 @@
 class Config
 {
 public:
-  static const byte PIN_INDICATOR = 6;
   static const byte PIN_ULTRASONIC_SENSOR_TRIGGER = 2;
   static const byte PIN_ULTRASONIC_SENSOR_ECHO = 3;
+  static const byte PIN_INDICATOR = 6;
+  static const byte PIN_LED = 7;
   static const byte PIN_BTN_CHECK = 8;
   static const byte PIN_MODEM_RX = 10;
   static const byte PIN_MODEM_TX = 11;
-  static const byte PIN_LED = 12;
 };
 
 #endif
