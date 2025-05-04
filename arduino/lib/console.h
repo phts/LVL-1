deeaf363573b03db660a2f524f3df9640d768223
Phil Tsaryk <phil@tsaryk.com>
2025-05-04 15:07:53 +0200

    refactor: string optimizations

--- arduino/lib/console.h
+++ arduino/lib/console.h
@@ -8,44 +8,58 @@ public:
   {
     Serial.println(str);
   }
-  void info(String str, String str2)
+  void info(const __FlashStringHelper *str)
   {
-    Serial.print(str + F(" "));
-    Serial.println(str2);
+    Serial.println(str);
   }
 #if DEBUG
-  void debug(String scope, String str)
+  void debug(const __FlashStringHelper *str)
+  {
+    Serial.println(str);
+  }
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *str)
   {
     Serial.print(scope);
     Serial.print(F(":: "));
     Serial.println(str);
   }
-  void debug(String scope, String str, String str2)
+  void debug(const __FlashStringHelper *scope, String str)
   {
     Serial.print(scope);
     Serial.print(F(":: "));
-    Serial.print(str + F(" "));
-    Serial.println(str2);
+    Serial.println(str);
   }
-  void debug(String scope, String str, int value)
+
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, String value)
   {
     Serial.print(scope);
     Serial.print(F(":: "));
-    Serial.print(str + F(" "));
+    Serial.print(label);
+    Serial.print(F(" "));
     Serial.println(value);
   }
-  void debug(String scope, String str, float value)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, int value)
   {
     Serial.print(scope);
     Serial.print(F(":: "));
-    Serial.print(str + F(" "));
+    Serial.print(label);
+    Serial.print(F(" "));
     Serial.println(value);
   }
-  void debug(String scope, String str, float array[], int len)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, float value)
   {
     Serial.print(scope);
     Serial.print(F(":: "));
-    Serial.print(str + F(" "));
+    Serial.print(label);
+    Serial.print(F(" "));
+    Serial.println(value);
+  }
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, float array[], int len)
+  {
+    Serial.print(scope);
+    Serial.print(F(":: "));
+    Serial.print(label);
+    Serial.print(F(" "));
     for (byte i = 0; i < len; i++)
     {
       Serial.print(array[i]);
@@ -54,19 +68,22 @@ public:
     Serial.println();
   }
 #else
-  void debug(String scope, String str)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *str)
   {
   }
-  void debug(String scope, String str, String str2)
+  void debug(const __FlashStringHelper *scope, String str)
   {
   }
-  void debug(String scope, String str, int value)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, String value)
   {
   }
-  void debug(String scope, String str, float value)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, int value)
   {
   }
-  void debug(String scope, String str, float array[], int len)
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, float value)
+  {
+  }
+  void debug(const __FlashStringHelper *scope, const __FlashStringHelper *label, float array[], int len)
   {
   }
 #endif
