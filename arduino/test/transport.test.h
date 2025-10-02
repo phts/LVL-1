b2a9271b6430c474ff110a1a21e17e2928725fc0
Phil Tsaryk <phil@tsaryk.com>
2025-10-02 12:56:21 +0200

    test(transport): align to transport API

--- arduino/test/transport.test.h
+++ arduino/test/transport.test.h
@@ -2,7 +2,9 @@
 #define transport_test_h
 
 #include "../lib/settings.h"
+#undef DEBUG
 #define DEBUG true
+#undef DEBUG_RXTX
 #define DEBUG_RXTX false
 
 #include <SoftwareSerial.h>
@@ -22,7 +24,7 @@ void onResponse(String resp)
 {
   Serial.println(String(F("<<<")) + resp);
 }
-void onFail(byte type, String desc)
+void onFail(String command, byte type, String desc)
 {
   Serial.println(String(F("<<<FAIL: ")) + String(type) + String(F(" - ")) + desc);
 }
@@ -38,7 +40,46 @@ void loop()
     String cmd = Serial.readString();
     cmd.trim();
     Serial.println(String(F(">>>")) + cmd);
-    transport.exec(cmd, onResponse, onFail);
+    if (cmd == F("!connect"))
+    {
+      transport.exec(F("!connect"), onResponse, onFail);
+    }
+    else if (cmd == F("!disconnect"))
+    {
+      transport.exec(F("!disconnect"), onResponse, onFail);
+    }
+    else if (cmd.startsWith(F("!level=")))
+    {
+      transport.exec(F("!level"), cmd.substring(cmd.indexOf(F("=")) + 1), onResponse, onFail);
+    }
+    else if (cmd.startsWith(F("!levelm=")))
+    {
+      transport.exec(F("!levelm"), cmd.substring(cmd.indexOf(F("=")) + 1), onResponse, onFail);
+    }
+    else if (cmd.startsWith(F("!log=")))
+    {
+      transport.exec(F("!log"), cmd.substring(cmd.indexOf(F("=")) + 1), onResponse, onFail);
+    }
+    else if (cmd == F("!rc"))
+    {
+      transport.exec(F("!rc"), onResponse, onFail);
+    }
+    else if (cmd == F("!healthcheck"))
+    {
+      transport.exec(F("!healthcheck"), onResponse, onFail);
+    }
+    else if (cmd == F("!fail"))
+    {
+      transport.exec(F("!fail=reason1"), onResponse, onFail);
+    }
+    else if (cmd == F("!ping"))
+    {
+      transport.exec(F("!ping"), onResponse, onFail);
+    }
+    else
+    {
+      transport.exec(F("!unknown"), onResponse, onFail);
+    }
   }
   transport.tick();
 }
